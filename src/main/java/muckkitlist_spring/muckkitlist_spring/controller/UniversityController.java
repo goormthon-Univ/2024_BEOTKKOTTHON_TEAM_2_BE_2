16645d89f227f69dd0db0d697d711826c72ac9c2
woody6624 <woody6624@gmail.com>
2024-03-18 15:52:48 +0900

    대학정보 서치쿼리 임시적용

--- src/main/java/muckkitlist_spring/muckkitlist_spring/controller/UniversityController.java
+++ src/main/java/muckkitlist_spring/muckkitlist_spring/controller/UniversityController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/universities")
 @Tag(name = "University", description = "대학교 관련 API")
@@ -28,5 +30,12 @@ public class UniversityController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping
+    @Operation(summary = "모든 대학교 목록 조회", description = "모든 대학교 정보를 조회합니다.")
+    public ResponseEntity<List<UniversityInfoEntity>> getAllUniversities() {
+        List<UniversityInfoEntity> universities = universityInfoService.getAllUniversities();
+        return ResponseEntity.ok(universities);
     }
 }
--- src/main/java/muckkitlist_spring/muckkitlist_spring/service/UniversityInfoService.java
+++ src/main/java/muckkitlist_spring/muckkitlist_spring/service/UniversityInfoService.java
@@ -1,11 +1,17 @@
 package muckkitlist_spring.muckkitlist_spring.service;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import muckkitlist_spring.muckkitlist_spring.entity.UniversityInfoEntity;
 import muckkitlist_spring.muckkitlist_spring.repository.UniversityInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
+@Data
 public class UniversityInfoService {
     @Autowired
     private UniversityInfoRepository universityInfoRepository;
@@ -14,5 +20,8 @@ public class UniversityInfoService {
         return universityInfoRepository.findByUniversityName(universityName);
     }
 
-
+    public List<UniversityInfoEntity> getAllUniversities() {
+        return universityInfoRepository.findAll();
+    }
 }
+
